99af7086d16582a5553f019f2bc5657b780a5555
MaximilianoPeña <maxitofinn@gmail.com>
2025-05-28 15:59:03 -0600

    Hice un cambio en la base de datos

--- Creacion_DB_cafe_internet.sql
+++ Creacion_DB_cafe_internet.sql
@@ -1,32 +1,22 @@
-/*Creacion de la base de datos de cafe internet */
 create database cafe_internet;
 use cafe_internet;
-create table Detalles_venta(
-	id_Detalles_venta  int primary key auto_increment not null,
-    id_Ventas int not null,
-    id_Productos int,
-    id_Snacks int ,
-    Cantidad int not null,
-    Nombre_producto varchar(50) not null,
-    Precio_unitario decimal not null,
-    Subtotal decimal not null,
-    foreign key (id_Productos) references Productos_Papeleria(id_Producto),
-    foreign key (id_Snacks) references Productos_Snacks(id_Snack)
+
+	CREATE TABLE Empleados(
+	id_Empleado	INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	RFC	VARCHAR(13),
+	Fecha_de_contratacion DATE,
+	Puesto	VARCHAR(30),
+	Sueldo	DOUBLE,
+	Turno	VARCHAR(15),
+	Estatus	VARCHAR(10) CHECK (Estatus IN ('activo', 'inactivo')),
+	Correo	VARCHAR(50),
+	Telefono VARCHAR(10) CHECK (Telefono REGEXP '^[0-9]+$'),
+	Nombre	VARCHAR(20),
+	Ap_paterno	VARCHAR(20),
+	Ap_materno	VARCHAR(20),
+	Sexo char(1) CHECK (Sexo IN ('h', 'm'))
 	);
-    
-create table Detalles_renta(
-	id_Detalles_renta int primary key auto_increment not null,
-    id_Renta int not null,
-    id_Dispositivos int not null,
-    Tipo enum("Consola","Control","Juego","Computadora") not null,
-    Cantidad int not null,
-    Horas_rentadas int not null,
-    Costo_de_renta decimal not null,
-    Subtotal decimal not null,
-    foreign key(id_Renta) references Renta_Dispositivos(id_Renta),
-    foreign key(id_Dispositivos) references Dispositivos (id_Dispositivo)
-	);
-    
+
     CREATE TABLE Dispositivos(
     id_Dispositivo INT PRIMARY KEY NOT NULL auto_increment,
     Nombre_dispositivo VARCHAR(30) NOT NULL,
@@ -35,6 +25,21 @@ create table Detalles_renta(
     Numero_serie VARCHAR(10)NOT NULL
     );
     
+    	CREATE TABLE Productos_Papeleria(
+	id_Producto	INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	Nombre_producto	VARCHAR(15) NOT NULL,
+	Descripcion	VARCHAR(15) NOT NULL,
+	Precio_unitario	DOUBLE NOT NULL,
+	Marca VARCHAR(15) NOT NULL
+);
+
+	CREATE TABLE Productos_Snacks(
+	id_Snack INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	Nombre_producto	VARCHAR(15) NOT NULL,
+	Descripcion	VARCHAR(15) NOT NULL,
+	Precio_unitario	DOUBLE NOT NULL,
+	Marca VARCHAR(15) NOT NULL
+);
 	CREATE TABLE Renta_Dispositivos(
     id_Renta INT PRIMARY KEY NOT NULL auto_increment,
     id_Empleados INT NOT NULL,
@@ -57,38 +62,6 @@ create table Detalles_renta(
     foreign key(id_Productos_snacks) references Productos_Snacks(Id_Snack)
 	);
     
-    CREATE TABLE Empleados(
-	id_Empleado	INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-	RFC	VARCHAR(13),
-	Fecha_de_contratacion DATE,
-	Puesto	VARCHAR(30),
-	Sueldo	DOUBLE,
-	Turno	VARCHAR(15),
-	Estatus	VARCHAR(10) CHECK (Estatus IN ('activo', 'inactivo')),
-	Correo	VARCHAR(50),
-	Telefono VARCHAR(10) CHECK (Telefono REGEXP '^[0-9]+$'),
-	Nombre	VARCHAR(20),
-	Ap_paterno	VARCHAR(20),
-	Ap_materno	VARCHAR(20),
-	Sexo char(1) CHECK (Sexo IN ('h', 'm'))
-);
-
-	CREATE TABLE Productos_Papeleria(
-	id_Producto	INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-	Nombre_producto	VARCHAR(15) NOT NULL,
-	Descripcion	VARCHAR(15) NOT NULL,
-	Precio_unitario	DOUBLE NOT NULL,
-	Marca VARCHAR(15) NOT NULL
-);
-
-	CREATE TABLE Productos_Snacks(
-	id_Snack INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-	Nombre_producto	VARCHAR(15) NOT NULL,
-	Descripcion	VARCHAR(15) NOT NULL,
-	Precio_unitario	DOUBLE NOT NULL,
-	Marca VARCHAR(15) NOT NULL
-);
-
 	create table Ventas(
     id_Venta int primary key auto_increment not null,
     id_Empleado int not null,
@@ -97,4 +70,29 @@ create table Detalles_renta(
     Metodo_de_pago enum("Efectivo","Tarjeta") not null,
     foreign key(id_Empleado) references Empleados(Id_Empleado)
     );
+    create table Detalles_venta(
+	id_Detalles_venta  int primary key auto_increment not null,
+    id_Ventas int not null,
+    id_Productos int,
+    id_Snacks int ,
+    Cantidad int not null,
+    Nombre_producto varchar(50) not null,
+    Precio_unitario decimal not null,
+    Subtotal decimal not null,
+    foreign key (id_Productos) references Productos_Papeleria(id_Producto),
+    foreign key (id_Snacks) references Productos_Snacks(id_Snack),
+    foreign key (id_Ventas) references Ventas(id_Venta)
+	);
     
+create table Detalles_renta(
+	id_Detalles_renta int primary key auto_increment not null,
+    id_Renta int not null,
+    id_Dispositivos int not null,
+    Tipo enum("Consola","Control","Juego","Computadora") not null,
+    Cantidad int not null,
+    Horas_rentadas int not null,
+    Costo_de_renta decimal not null,
+    Subtotal decimal not null,
+    foreign key(id_Renta) references Renta_Dispositivos(id_Renta),
+    foreign key(id_Dispositivos) references Dispositivos (id_Dispositivo)
+	);
