b0b8cfc1132f3d915ab997bf65b7a21bc2ab31a0
Maxitofinn <maxitofinn@gmail.com>
2025-05-28 00:11:17 -0600

    registros
    aqui se agregaran los registros de todos

---
+++ Registros_todos.sql
@@ -0,0 +1,13 @@
+/*Espacio para agregar registros de Alan */
+
+
+
+/*Espacio para agregar registros de Aaron */
+
+
+
+/*Espacio para agregar registros de Jair */
+
+
+
+/*Espacio para agregar registros de Max */
