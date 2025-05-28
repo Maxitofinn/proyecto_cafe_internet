a1969b69689d4578226b237c7a22629672ef670a
resre26 <reresreguobs7@gmail.com>
2025-05-28 17:05:18 -0600

    Hice un cambio en la rama main

--- Registros_todos.sql
+++ Registros_todos.sql
@@ -1,6 +1,61 @@
 /*Espacio para agregar registros de Alan */
 /*agregando tablas de ALAN(Tabla Inventario)*/
+INSERT INTO Inventario (Nombre_producto, Stock, Fecha) VALUES
+-- Consolas (6)
+('PlayStation 5', 3, '2025-01-15 08:30:00'),
+('PlayStation 4', 4, '2025-01-15 08:30:00'),
+('Xbox Series X', 2, '2025-01-15 08:30:00'),
+('Xbox One', 3, '2025-01-15 08:30:00'),
+('Xbox 360', 2, '2025-01-15 08:30:00'),
+('Nintendo Switch', 4, '2025-01-15 08:30:00'),
 
+-- Controles (6)
+('Control PS5 DualSense', 6, '2025-01-15 08:30:00'),
+('Control PS4 DualShock', 8, '2025-01-15 08:30:00'),
+('Control Xbox Series X', 5, '2025-01-15 08:30:00'),
+('Control Xbox One', 6, '2025-01-15 08:30:00'),
+('Control Xbox 360', 4, '2025-01-15 08:30:00'),
+('Joy-Con Nintendo Switch', 7, '2025-01-15 08:30:00'),
+
+-- Juegos (15)
+('FIFA 25 PS5', 3, '2025-01-15 08:30:00'),
+('The Last of Us Part III PS5', 4, '2025-01-15 08:30:00'),
+('God of War: Valhalla PS5', 2, '2025-01-15 08:30:00'),
+('Spider-Man 2 PS5', 3, '2025-01-15 08:30:00'),
+('Uncharted: Legacy Collection PS5', 3, '2025-01-15 08:30:00'),
+('Halo Infinite: Reborn Xbox', 3, '2025-01-15 08:30:00'),
+('Forza Horizon 6 Xbox', 2, '2025-01-15 08:30:00'),
+('Gears 6 Xbox', 3, '2025-01-15 08:30:00'),
+('Starfield: Extended Xbox', 2, '2025-01-15 08:30:00'),
+('Mario Kart X Switch', 3, '2025-01-15 08:30:00'),
+('The Legend of Zelda: Echoes of Time Switch', 4, '2025-01-15 08:30:00'),
+('Super Smash Bros. Ultimate DX Switch', 3, '2025-01-15 08:30:00'),
+('Animal Crossing: New Frontiers Switch', 2, '2025-01-15 08:30:00'),
+('Call of Duty: Black Ops V Xbox', 3, '2025-01-15 08:30:00'),
+('Gran Turismo 8 PS5', 2, '2025-01-15 08:30:00'),
+
+-- Computadoras (1)
+('Computadora', 8, '2025-01-15 08:30:00'),
+
+-- Productos Papelería (6)
+('Lápiz HB', 50, '2025-01-15 08:30:00'),
+('Cuaderno cuadrícula', 30, '2025-01-15 08:30:00'),
+('Borrador blanco', 40, '2025-01-15 08:30:00'),
+('Pluma azul', 45, '2025-01-15 08:30:00'),
+('Resistol pegamento', 15, '2025-01-15 08:30:00'),
+('Hojas blancas', 20, '2025-01-15 08:30:00'),
+
+-- Snacks (10)
+('Coca-Cola 600ml', 48, '2025-01-15 08:30:00'),
+('Sabritas Original', 36, '2025-01-15 08:30:00'),
+('Jugo Jumex Mango', 24, '2025-01-15 08:30:00'),
+('Agua Bonafont 1L', 60, '2025-01-15 08:30:00'),
+('Galletas Oreo', 28, '2025-01-15 08:30:00'),
+('Papas Ruffles', 32, '2025-01-15 08:30:00'),
+('Mazapan', 40, '2025-01-15 08:30:00'),
+('Pulparindo', 50, '2025-01-15 08:30:00'),
+('Chocolate Abuelita', 30, '2025-01-15 08:30:00'),
+('Cacahuates Japoneses', 25, '2025-01-15 08:30:00');
 
 /*Espacio para agregar registros de Aaron */
 /*(Tablas Productos_Papeleria,Productos_Snacks,Empleados) */
